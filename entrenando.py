5888c04af07b4752320c03c489c8adba352eaec5
AlvaroSudo <aaqr1@alu.ua.es>
2025-11-30 01:16:57 +0100

    First code committed

---
+++ entrenando.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Cargar el modelo YOLOv8 preentrenado
+model = YOLO('pesos_train.pt')
+
+# Run inference on the source 
+results =model(source=0, show=True, conf=0.3, save=True) #generator of Results
